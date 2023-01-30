9f0956809b89ae699e20785047f937b104cae403
Linus <git@linus9000.de>
2023-01-30 18:58:15 +0100

    add log message

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,13 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class LuckyController extends AbstractController
 {
     #[Route('/lucky/number')]
-    public function number(): Response
+    public function number(LoggerInterface $logger): Response
     {
         $number = random_int(0, 100);
+        $logger->info("Number: {$number}");
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
